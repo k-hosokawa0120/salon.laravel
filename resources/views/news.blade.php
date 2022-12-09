befaaad9f78c9795a47a1efd8bac7287d71dc8e7
r-igarashi23 <a13cd001@gmail.com>
2022-12-09 23:06:18 +0900

    fix

---
+++ resources/views/news.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('/css/reset.css') }}" >
+    <link rel="stylesheet" href="{{ asset('/css/css.css') }}" >
+    <title>Laravel</title>
+    <style>
+    </style>
+  </head>
+<body>
+  <section class="news">
+    <div class="section__title-wrap">
+      <div class="section__title">NEWS</div>
+    </div>
+    <dl class="news__list">
+      <div class="news__item">
+        <dt class="news__term">2017-8-20</dt>
+        <dd class="news__data">WEB限定価格のお知らせ</dd>
+      </div>
+      <div class="news__item">
+        <dt class="news__term">2017-8-20</dt>
+        <dd class="news__data">WEB限定価格のお知らせ</dd>
+      </div>
+    </dl>
+  </section>
+</body>
+</html>
